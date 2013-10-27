9e3872adc5baed37c4899377336573c69c01e100
Magicalex <magicalex14000@gmail.com>
2013-10-27 16:17:30 +0100

    Ajout fonction delete conf user
    déplacement admin gestion user dans paramètre.

--- html/admin.php
+++ html/admin.php
@@ -1,14 +1,20 @@
     <!-- CONTENEUR -->
     <div class="container marg">
-        <h1 class="page-header dashboard"><i class="glyphicon glyphicon-wrench"></i> Paramètres de <?php if (isset($_GET['u'])) echo $_GET['u']; ?></h1>
+        <h1 class="page-header dashboard"><i class="glyphicon glyphicon-wrench"></i> Configuration</h1>
         
         
         <section class="row">
             <article class="col-md-12">
                 <div class="well well-sm">
-                    <h4 class="titre-head"></h4>
+                    <h4 class="titre-head">  Configuration de l'utilisateur <?php if (!empty($_GET['u'])) echo $_GET['u']; ?></h4>
                     <div class="trait"></div>
-                    <p>users : </p>  
+                    <p>users : </p>
+                    
+                    <form>
+
+
+                    </form>
+
                 </div>
             </article>
         </section>
