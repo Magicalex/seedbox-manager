cb81cdca9a8db2a1917cde52f45457d97f097087
Magicalex <magicalex14000@gmail.com>
2013-11-03 16:43:30 +0100

    fix bug responsive table
    + modification du css pour les formulaire

--- html/option.php
+++ html/option.php
@@ -58,26 +58,32 @@
                     <div class="trait"></div>
 
                     <form method="post" action="?option" role="form">
-                        <div class="checkbox">
-                            <input type="checkbox" name="active_bloc_info" value="true" id="active_bloc_info" <?php if ($user->blocInfo() === true) echo 'checked'; ?> ><label for="active_bloc_info"><span class="ui"></span> Activer le bloc information utilisateur</label>
-                        </div>
-                        <div class="checkbox">
-                            <input type="checkbox" name="active_ftp" value="true" id="active_ftp" <?php if ($user->blocFtp() === true) echo 'checked'; ?> ><label for="active_ftp"><span class="ui"></span> Activer le bloc accès ftp / sftp / transdroid</label>
-                        </div>
-                        <div class="checkbox">
-                            <input type="checkbox" name="active_reboot" value="true" id="active_reboot" <?php if ($user->blocRtorrent() === true) echo 'checked'; ?> ><label for="active_reboot"><span class="ui"></span> Activer le bloc gestion de rtorrent</label>
-                        </div>
-                        <div class="checkbox">
-                            <input type="checkbox" name="active_support" value="true" id="active_support" <?php if ($user->blocSupport() === true) echo 'checked'; ?> ><label for="active_support"><span class="ui"></span> Activer le bloc support</label>
-                        </div>
-                        <div class="form-group">
-                            <label for="url-deconnexion">Url de redirection à la déconnexion</label>
-                            <div class="row">
-                                <div class="col-lg-7">
-                                    <input type="url" name="url_redirect" class="form-control" id="url-deconnexion" value="<?php echo $user->url_redirect(); ?>">
+                        <fieldset>
+                            <legend>Paramètre page d'accueil</legend>
+                            <div class="checkbox">
+                                <input type="checkbox" name="active_bloc_info" value="true" id="active_bloc_info" <?php if ($user->blocInfo() === true) echo 'checked'; ?> ><label for="active_bloc_info"><span class="ui"></span> Activer le bloc information utilisateur</label>
+                            </div>
+                            <div class="checkbox">
+                                <input type="checkbox" name="active_ftp" value="true" id="active_ftp" <?php if ($user->blocFtp() === true) echo 'checked'; ?> ><label for="active_ftp"><span class="ui"></span> Activer le bloc accès ftp / sftp / transdroid</label>
+                            </div>
+                            <div class="checkbox">
+                                <input type="checkbox" name="active_reboot" value="true" id="active_reboot" <?php if ($user->blocRtorrent() === true) echo 'checked'; ?> ><label for="active_reboot"><span class="ui"></span> Activer le bloc gestion de rtorrent</label>
+                            </div>
+                            <div class="checkbox">
+                                <input type="checkbox" name="active_support" value="true" id="active_support" <?php if ($user->blocSupport() === true) echo 'checked'; ?> ><label for="active_support"><span class="ui"></span> Activer le bloc support</label>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <legend>Paramètre de déconnexion</legend>
+                            <div class="form-group">
+                                <label for="url-deconnexion">Url de redirection à la déconnexion</label>
+                                <div class="row">
+                                    <div class="col-lg-7">
+                                        <input type="url" name="url_redirect" class="form-control" id="url-deconnexion" value="<?php echo $user->url_redirect(); ?>">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </fieldset>
                         <p class="text-right fix-marg-input">
                             <input type="hidden" name="simple_conf_user">
                             <input type="submit" name="submit" value="Enregistrer" class="btn btn-info">
@@ -92,36 +98,38 @@
             <div class="well well-sm">
                 <h4 class="titre-head"><i class="glyphicon glyphicon-th-list"></i> Administration : gestion utilisateurs</h4>
                 <div class="trait"></div>
-                <table class="table table-bordered table-striped">
-                    <tr>
-                        <td><strong>#</strong></td>
-                        <td><strong>Utilisateur</strong></td>
-                        <td><strong>Modifier</strong></td>
-                        <td><strong>Supprimer</strong></td>
-                    </tr>
-                    <?php $i = 0; $num_user = 1; foreach (Users::get_all_users() as $i => $user_name) { ?>
-                    <tr>
-                        <td><?php echo $num_user; ?></td>
-                        <td><?php echo $user_name; ?></td>
-                        <td>
-                            <a href="?option&amp;user=<?php echo $user_name; ?>" class="btn btn-default btn-xs edit-btn-user">
-                                <i class="glyphicon glyphicon-edit"></i> éditer
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <tr>
+                            <td><strong>#</strong></td>
+                            <td><strong>Utilisateur</strong></td>
+                            <td><strong>Modifier</strong></td>
+                            <td><strong>Supprimer</strong></td>
+                        </tr>
+                        <?php $i = 0; $num_user = 1; foreach (Users::get_all_users() as $i => $user_name) { ?>
+                        <tr>
+                            <td><?php echo $num_user; ?></td>
+                            <td><?php echo $user_name; ?></td>
+                            <td>
+                                <a href="?option&amp;user=<?php echo $user_name; ?>" class="btn btn-default btn-xs edit-btn-user">
+                                    <i class="glyphicon glyphicon-edit"></i> éditer
+                                </a>
+                            </td>
+                            <td>
+                            <?php if ( $userName != $user_name) { ?>
+                            <a data-toggle="modal" class="popup-delete-user btn btn-danger btn-xs" data-user="<?php echo $user_name ?>" href="#delete-user">
+                                <i class="glyphicon glyphicon-trash"></i> supprimer
                             </a>
-                        </td>
-                        <td>
-                        <?php if ( $userName != $user_name) { ?>
-                        <a data-toggle="modal" class="popup-delete-user btn btn-danger btn-xs" data-user="<?php echo $user_name ?>" href="#delete-user">
-                            <i class="glyphicon glyphicon-trash"></i> supprimer
-                        </a>
-                        <?php } else { ?>
-                        <a class="popup-delete-user btn btn-danger btn-xs disabled">
-                            <i class="glyphicon glyphicon-trash"></i> supprimer
-                        </a>
-                        <?php } ?>
-                        </td>
-                    </tr>
-                    <?php $num_user++; } ?>
-                </table>
+                            <?php } else { ?>
+                            <a class="popup-delete-user btn btn-danger btn-xs disabled">
+                                <i class="glyphicon glyphicon-trash"></i> supprimer
+                            </a>
+                            <?php } ?>
+                            </td>
+                        </tr>
+                        <?php $num_user++; } ?>
+                    </table>
+                </div>
             </div>
         </article>
 
--- js/app.js
+++ js/app.js
@@ -1,62 +1,31 @@
-( function () {
+/* code jquery for mod boostrap and more */
 
-    //code activation tooltip
-    $('.load-average').tooltip({
-        placement: 'top',
-        trigger: 'hover'
-    }).tooltip('hide');
+$('.load-average').tooltip({
+    placement: 'top',
+    trigger: 'hover'
+}).tooltip('hide');
 
-} ) ();
-
-( function () {
-
-    //code activation popover
-    $('#popupfilezilla').popover({
-        html : true,
-        trigger : 'hover',
-        content : ' &bull; Cliquer sur ce bouton.<br> &bull; Ouvrir filezilla.<br> &bull; Fichier -> Importer les paramètres.<br> &bull; Sélectionner le fichier filezilla.xml.<br> &bull; C\'est terminé !',
-        title : 'Configurer filezilla rapidement !',
-        placement : function() {
-            var width = $(window).width();
-            return width >= 979 ? 'right' : 'top';
-        }
-    });
-
-    $('#popuptransdroid').popover({
-        html : true,
-        trigger : 'hover',
-        content : 'Fonctionnalité en cour de développement.<br>La génération du fichier de configuration fonctionne pour ceux qui sont à l\'aise sous android peuvent le copier/coller via leur pc.',
-        title : 'Configurer transdroid rapidement !',
-        placement : function() {
-            var width = $(window).width();
-            return width >= 979 ? 'right' : 'top';
-        }
-    });
-
-} ) ();
-
-( function() {
-    
-    if ( document.getElementById("blockInfo") && document.getElementById("blockFtp") && document.getElementById("blockRtorrent") && document.getElementById("blockSupport") )
-    {
-        // ajustement du style css des blocs
-        var blockInfo = document.getElementById("blockInfo");
-        var blockFtp = document.getElementById("blockFtp");
-        var blockRtorrent = document.getElementById("blockRtorrent");
-        var blockSupport = document.getElementById("blockSupport");
-
-        if(blockInfo.offsetHeight > blockFtp.offsetHeight)
-            blockFtp.style.height = blockInfo.offsetHeight+"px";
-        else
-            blockInfo.style.height = blockFtp.offsetHeight+"px";
-
-        if(blockRtorrent.offsetHeight > blockSupport.offsetHeight)
-            blockSupport.style.height = blockRtorrent.offsetHeight+"px";
-        else
-            blockRtorrent.style.height = blockSupport.offsetHeight+"px";
+$('#popupfilezilla').popover({
+    html : true,
+    trigger : 'hover',
+    content : ' &bull; Cliquer sur ce bouton.<br> &bull; Ouvrir filezilla.<br> &bull; Fichier -> Importer les paramètres.<br> &bull; Sélectionner le fichier filezilla.xml.<br> &bull; C\'est terminé !',
+    title : 'Configurer filezilla rapidement !',
+    placement : function() {
+        var width = $(window).width();
+        return width >= 979 ? 'right' : 'top';
     }
+});
 
-} ) ();
+$('#popuptransdroid').popover({
+    html : true,
+    trigger : 'hover',
+    content : 'Fonctionnalité en cour de développement.<br>La génération du fichier de configuration fonctionne pour ceux qui sont à l\'aise sous android peuvent le copier/coller via leur pc.',
+    title : 'Configurer transdroid rapidement !',
+    placement : function() {
+        var width = $(window).width();
+        return width >= 979 ? 'right' : 'top';
+    }
+});
 
 $(document).on('click', '.popup-delete-user', function ()
 {
@@ -65,4 +34,23 @@ $(document).on('click', '.popup-delete-user', function ()
     $('#target-delete-user').html('<i class="glyphicon glyphicon-trash"></i> Suppression de l\'utilisateur : <strong>'+userName+'</strong>');
 });
 
-$('.alert-success').delay(4500).hide('slow');
+$('.alert-success').delay(4000).hide('slow');
+
+   
+if ( document.getElementById("blockInfo") && document.getElementById("blockFtp") && document.getElementById("blockRtorrent") && document.getElementById("blockSupport") )
+{
+    var blockInfo = document.getElementById('blockInfo');
+    var blockFtp = document.getElementById('blockFtp');
+    var blockRtorrent = document.getElementById('blockRtorrent');
+    var blockSupport = document.getElementById('blockSupport');
+
+    if(blockInfo.offsetHeight > blockFtp.offsetHeight)
+        blockFtp.style.height = blockInfo.offsetHeight+'px';
+    else
+        blockInfo.style.height = blockFtp.offsetHeight+'px';
+
+    if(blockRtorrent.offsetHeight > blockSupport.offsetHeight)
+        blockSupport.style.height = blockRtorrent.offsetHeight+'px';
+    else
+        blockRtorrent.style.height = blockSupport.offsetHeight+'px';
+}
