9feff27e896586fd795b79f9602f070d52e41653
magicalex <magicalex14000@gmail.com>
2014-10-10 04:52:00 +0200

    bug fix function logout #3
     fix for the Urls with /uri/

--- public/themes/default/js/app.js
+++ public/themes/default/js/app.js
@@ -1,78 +1,87 @@
-/* code jquery for mod bootstrap and more */
+(function($){
 
-$('.load-average').tooltip(
-{
-    placement: 'top',
-    trigger: 'hover'
-}).tooltip('hide');
+    /* code jquery for mod bootstrap and more */
 
-$('#popupfilezilla').popover(
-{
-    html : true,
-    trigger : 'hover',
-    content : ' &bull; Cliquer sur ce bouton.<br> &bull; Ouvrir filezilla.<br> &bull; Fichier -> Importer les paramètres.<br> &bull; Sélectionner le fichier filezilla.xml.<br> &bull; C\'est terminé !',
-    title : 'Configurer filezilla rapidement !',
-    placement : function()
+    $('.load-average').tooltip(
     {
-        var width = $(window).width();
-        return width >= 979 ? 'right' : 'top';
-    }
-});
+        placement: 'top',
+        trigger: 'hover'
+    }).tooltip('hide');
 
-$('#popuptransdroid').popover(
-{
-    html : true,
-    trigger : 'hover',
-    content : "Génère un fichier de configuration pour l'application transdroid.",
-    title : 'Configurer transdroid rapidement !',
-    placement : function()
+    $('#popupfilezilla').popover(
     {
-        var width = $(window).width();
-        return width >= 979 ? 'right' : 'top';
-    }
-});
-
-/* Envoie les donnees de supp au modal */
-$('.popup-delete-user').click( function()
-{
-    var userName = $(this).data('user');
-    $('#deleteUserName').val(userName); // id input modal delete user add value="userName"
-    $('#target-delete-user').html('<i class="glyphicon glyphicon-trash"></i> Suppression de l\'utilisateur : <strong>'+userName+'</strong>');
-});
-
-/* Cache les messages succes au bout de 4 sec */
-$('.alert-success').delay(4000).hide('slow');
-
-/* Cache (au demarrage) et affiche un ticket */
-$('.ticket').hide();
-$('.show-ticket').click( function ()
-{
-    $('.ticket-' + this.id).fadeIn();
-});
-
-/* Cache un ticket affiche */
-$('.close-ticket').click( function ()
-{
-    $('.ticket-' + this.id).fadeOut();
-});
-
-/* function LOGOUT */
-$('#logout').click( function()
-{
-    var UrlRedirect = $(this).data('urlredirect');
-    var host = $(this).data('host');
-    $.get('//logout@' + host);
-    $.loader(
-    {
-        id: 'jquery-loader',
-        className: '',
-        content: '<span>Déconnexion...</span>',
-        height: 60,
-        width: 200,
-        background: {id:'jquery-loader-background'}
+        html : true,
+        trigger : 'hover',
+        content : ' &bull; Cliquer sur ce bouton.<br> &bull; Ouvrir filezilla.<br> &bull; Fichier -> Importer les paramètres.<br> &bull; Sélectionner le fichier filezilla.xml.<br> &bull; C\'est terminé !',
+        title : 'Configurer filezilla rapidement !',
+        placement : function()
+        {
+            var width = $(window).width();
+            return width >= 979 ? 'right' : 'top';
+        }
     });
-    setTimeout( function()
+
+    $('#popuptransdroid').popover(
     {
-        window.location.href = UrlRedirect;
-    }, 1000);
-});
+        html : true,
+        trigger : 'hover',
+        content : "Génère un fichier de configuration pour l'application transdroid.",
+        title : 'Configurer transdroid rapidement !',
+        placement : function()
+        {
+            var width = $(window).width();
+            return width >= 979 ? 'right' : 'top';
+        }
+    });
+
+    /* Envoie les donnees de supp au modal */
+    $('.popup-delete-user').click( function()
+    {
+        var userName = $(this).data('user');
+        $('#deleteUserName').val(userName); // id input modal delete user add value="userName"
+        $('#target-delete-user').html('<i class="glyphicon glyphicon-trash"></i> Suppression de l\'utilisateur : <strong>'+userName+'</strong>');
+    });
+
+    /* Cache les messages succes au bout de 4 sec */
+    $('.alert-success').delay(4000).hide('slow');
+
+    /* Cache (au demarrage) et affiche un ticket */
+    $('.ticket').hide();
+    $('.show-ticket').click( function ()
+    {
+        $('.ticket-' + this.id).fadeIn();
+    });
+
+    /* Cache un ticket affiche */
+    $('.close-ticket').click( function ()
+    {
+        $('.ticket-' + this.id).fadeOut();
+    });
+
+    /* function LOGOUT */
+    $('#logout').click( function()
+    {
+        var UrlRedirect = $(this).data('urlredirect');
+
+        var protocol = window.location.protocol;
+        var hostname = window.location.hostname;
+        var uri      = window.location.pathname;
+
+        $.get( protocol + '//logout@' + hostname + uri);
+
+        $.loader(
+        {
+            id: 'jquery-loader',
+            className: '',
+            content: '<span>Déconnexion...</span>',
+            height: 60,
+            width: 200,
+            background: {id:'jquery-loader-background'}
+        });
+        setTimeout( function()
+        {
+            window.location.href = UrlRedirect;
+        }, 1000);
+    });
+
+})(jQuery);
