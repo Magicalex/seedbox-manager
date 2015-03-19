4d4c8260a21193dee65300a2310155e0e7442adf
magicalex <magicalex14000@gmail.com>
2015-03-20 00:07:58 +0100

    fix : erreur déclaration de variable

--- source-reboot-rtorrent/start_rtorrent.c
+++ source-reboot-rtorrent/start_rtorrent.c
@@ -5,7 +5,7 @@ void start_rtorrent (char nickname[])
     //Déclarations
     char chaine [100] = {0};
 
-    snprintf(chaine, 100, "su --command='screen -dmS %s-rtorrent rtorrent' %s\n", nickname);
+    snprintf(chaine, 100, "su --command='screen -dmS %s-rtorrent rtorrent' %s\n", nickname, nickname);
     printf("%s", chaine);
     system (chaine);
 }
