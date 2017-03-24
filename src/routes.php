4cc4c1cade6085430d9814d814d53f5891d9b956
Magicalex <magicalex@mondedie.fr>
2017-03-24 07:40:19 +0100

    chore(): add parameter in url

--- src/routes.php
+++ src/routes.php
@@ -8,12 +8,12 @@ $app->group('/', function () {
     $this->get('', '\App\Controller\HomeController:index');
     $this->get('settings', '\App\Controller\HomeController:settings');
     $this->get('admin', '\App\Controller\AdminController:index')->add(new isAdmin());
-    $this->get('admin/{username}', '\App\Controller\AdminController:user')->add(new isAdmin());
+    $this->get('admin/{username:[a-z]+}', '\App\Controller\AdminController:user')->add(new isAdmin());
+    $this->get('download/{file:[a-z]+}', '\App\Controller\DownloadController:download');
     $this->post('reboot-rtorrent', '\App\Controller\HomeController:reboot');
     $this->post('settings/update', '\App\Controller\HomeController:update');
-    $this->post('admin/update/{username}', '\App\Controller\AdminController:update')->add(new isAdmin());
+    $this->post('admin/update/{username:[a-z]+}', '\App\Controller\AdminController:update')->add(new isAdmin());
     $this->post('admin/delete', '\App\Controller\AdminController:delete')->add(new isAdmin());
-})->add(new checkInstall());//->add(new isAuth());
+})->add(new checkInstall())->add(new isAuth());
 
 $app->get('/install', '\App\Controller\InstallController:index');
-$app->get('/download', '\App\Controller\DownloadController:download');
